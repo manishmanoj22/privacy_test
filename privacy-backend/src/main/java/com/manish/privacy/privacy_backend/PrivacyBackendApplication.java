783d042c973b228f3f8999906f0a192f4e57e449
manishp <manishmanoj22@gmail.com>
2025-06-15 13:57:53 +0200

    Initial commit with frontend and backend

---
+++ privacy-backend/src/main/java/com/manish/privacy/privacy_backend/PrivacyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.manish.privacy.privacy_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrivacyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrivacyBackendApplication.class, args);
+	}
+
+}
---
+++ privacy-backend/src/test/java/com/manish/privacy/privacy_backend/PrivacyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manish.privacy.privacy_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrivacyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
