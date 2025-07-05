be3dd8cdb2279a51f8a19fa64c94c076c29777a9
manishp <manishmanoj22@gmail.com>
2025-07-05 22:17:36 +0200

    Included contact form and changes in other pages to accomodate the form, along with postgreSQL table.

--- privacy-backend/src/main/java/com/manish/privacy/privacy_backend/PrivacyBackendApplication.java
+++ privacy-backend/src/main/java/com/manish/privacy/privacy_backend/PrivacyBackendApplication.java
@@ -10,7 +10,7 @@ public class PrivacyBackendApplication {
 
 		SpringApplication.run(PrivacyBackendApplication.class, args);
 
-		System.out.println("Hello, World!");
+		System.out.println("Hello, World1!");
 	}
 
 }
