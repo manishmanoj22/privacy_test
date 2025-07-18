7a526c87e15a0fe43f0f14668418d3c359d0eebf
manishp <manishmanoj22@gmail.com>
2025-07-19 00:37:04 +0200

    password setter error fix

--- privacy-backend/src/main/java/com/manish/privacy/privacy_backend/entity/LoginData.java
+++ privacy-backend/src/main/java/com/manish/privacy/privacy_backend/entity/LoginData.java
@@ -27,7 +27,7 @@ public class LoginData {
     public void setEmail(String email) {
         this.email = email;
     }
-    public void setPassword(String message) {
+    public void setPassword(String password) {
         this.password = password;
     }
 }
